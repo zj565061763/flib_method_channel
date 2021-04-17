7d387a40ec34a7e44942265faf04618787f69d3b
zjun <565061763@qq.com>
2021-04-17 16:12:28 +0800

    FMethodChannel

--- lib/src/core.dart
+++ lib/src/core.dart
@@ -2,38 +2,43 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+/// 原生平台方法触发回调
 typedef dynamic FMethodCallHandler(dynamic arguments);
 
 class FMethodChannel {
+  /// 全局通道
   static final FMethodChannel global = _FGlobalMethodChannel();
 
   final MethodChannel _methodChannel;
   final Map<String, FMethodCallHandler> _mapCallHandler = {};
 
   FMethodChannel(String name)
-      : assert(name != null && name.isNotEmpty),
+      : assert(name.isNotEmpty),
         this._methodChannel = MethodChannel('flutter.fanwe.com/' + name) {
     _methodChannel.setMethodCallHandler(_onMethodCall);
   }
 
+  /// 原生平台方法触发回调
   Future<dynamic> _onMethodCall(MethodCall call) async {
-    final FMethodCallHandler handler = _mapCallHandler[call.method];
+    final FMethodCallHandler? handler = _mapCallHandler[call.method];
     return handler == null ? null : handler(call.arguments);
   }
 
-  /// 监听某个方法触发
-  void listen(String method, FMethodCallHandler handler) {
-    assert(method != null && method.isNotEmpty);
-
-    if (handler != null) {
-      _mapCallHandler[method] = handler;
-    } else {
-      _mapCallHandler.remove(method);
-    }
+  /// 监听原生平台方法
+  void subscribe(String method, FMethodCallHandler handler) {
+    assert(method.isNotEmpty);
+    _mapCallHandler[method] = handler;
   }
 
-  /// 调用某个方法
-  Future<T> invokeMethod<T>(String method, [dynamic arguments]) {
+  /// 取消监听原生平台方法
+  void unsubscribe(String method) {
+    assert(method.isNotEmpty);
+    _mapCallHandler.remove(method);
+  }
+
+  /// 调用原生平台方法
+  Future<T?> invokeMethod<T>(String method, [dynamic arguments]) {
+    assert(method.isNotEmpty);
     return _methodChannel.invokeMethod(method, arguments);
   }
 
